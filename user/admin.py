c35c8c42957159177db98520faa94f7b91a9bb3d
skiboorg <d@skib.org>
2021-12-17 12:20:37 +0300

    add user fields

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import *
 
-admin.site.register(User)
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ( 'email',
+                    'fio',
+                    'phone',
+                    'own_ref_code',
+                    'used_ref_code',
+                    'balance',
+                    'ref_bonuses',
+                    'total_spend',
+                    )
+    ordering = ('id',)
+    search_fields = ('email', 'fio', 'phone', 'own_ref_code', 'used_ref_code',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info',
+         {'fields': (
+                    'fio',
+                    'phone',
+                    'own_ref_code',
+                    'used_ref_code',
+                    'balance',
+                    'ref_bonuses',
+                    'total_spend',
+
+         )}
+         ),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser',)}),)
+
+admin.site.register(User,UserAdmin)
 admin.site.register(UserRefferalFirstLine)
 admin.site.register(UserRefferalSecondLine)
 admin.site.register(UserRefferalThirdLine)
